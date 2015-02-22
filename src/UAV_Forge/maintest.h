a9bcc37834abf80b39abddee36fde0ceae085270
cprijic_mac <chrisprijic@gmail.com>
2015-02-22 14:01:57 -0800

    maintest.h added for easier compilation.

---
+++ src/UAV_Forge/maintest.h
@@ -0,0 +1,10 @@
+// Copyright 2015 Christopher Prijic
+#ifndef SRC_UAV_FORGE_MAINTEST_H_
+#define SRC_UAV_FORGE_MAINTEST_H_
+
+
+int setup();
+void loop();
+
+
+#endif /* SRC_UAV_FORGE_MAINTEST_H_ */
